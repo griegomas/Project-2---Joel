0de7cc392e71ef023f1be02599278b0892c810eb
griegomas <38501062+griegomas@users.noreply.github.com>
2019-01-27 21:02:20 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -3,15 +3,15 @@
 var fs = require("fs");
 var path = require("path");
 var Sequelize = require("sequelize");
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
+var basename = path.basename(module.filename);
+var env = process.env.NODE_ENV || "development";
 var config = require(__dirname + "/../config/config.json")[env];
 var db = {};
 
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+  var sequelize = new Sequelize(process.env[config.use_env_variable], {dialect: 'mysql'});
 } else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  var sequelize = new Sequelize(config.database, config.username, config.password, config, {dialect: 'mysql'});
 }
 
 fs
